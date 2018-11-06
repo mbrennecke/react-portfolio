20048249dbb315ad37c7e7f822fb2221ad3a218b
Matthew Brennecke <mbrennecke10@gmail.com>
2018-11-05 20:03:45 -0700

    whittling the hard stuff down

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,51 +1,52 @@
 import React from 'react';
 import { Container, Row, Col } from "../../components/Grid";
 import Links from "../../utils/links.js";
+import { PortfolioBoxes, PortfolioBoxTitle } from "../../components/Portfolio";
 import "./Portfolio.css";
 
 const Portfolio = () => {
 
-	console.log(Links);
+	console.log(Links[0]);
+	const projects = Links[0][0];
 	
 	return (
 		<Container>
-			<Row>
-				<Col size="twelve columns ">
-					<div className="links" id="projects">
-						<h2>
-							Projects developed with a team
-						</h2>
-					</div>
-				</Col>
-			</Row>
-			<Row>
-				<Col size="twelve columns ">
-					<div className="links">
-						<h2>
-							Sites with a front end and back end
-						</h2>
-					</div>
-				</Col>
-			</Row>
-			<Row>
-				<Col size="twelve columns ">
-					<div className="links">
-						<h2>
-							Sites that utilize only a front end
-						</h2>
-					</div>
-				</Col>
-			</Row>
-			<Row>
-				<Col size="twelve columns ">
-					<div className="links">
-						<h2>
-							Apps that are only back end
-						</h2>
-						<p>(links go to video demonstrations)</p>
-					</div>
-				</Col>
-			</Row>
+			<PortfolioBoxes className="links" id="projects">
+				<PortfolioBoxTitle>
+					Projects developed with a team
+				</PortfolioBoxTitle>
+				<Row>
+					<Col size="one-third column">
+						<a href= {`${projects.link}`} target="_blank" rel="noopener noreferrer"><img src={`/images/${projects.image}`} className="u-max-full-width image" alt={`${projects.title}`}/></a>
+						<div className="siteLinks">
+							<h3 className="site">{projects.title}</h3>
+							<h4 className="repo">-Github-</h4>
+						</div>
+					</Col>
+					<Col size="one-third column">
+						<img src="/images/pencilme.jpg" className="u-max-full-width"/>
+					</Col>
+					<Col size="one-third column">
+						<img src="/images/readlocal.jpg" className="u-max-full-width"/>
+					</Col>
+				</Row>
+			</PortfolioBoxes>
+			<PortfolioBoxes className="links" id="database">
+				<PortfolioBoxTitle>
+					Sites that utilize a front end and back end
+				</PortfolioBoxTitle>
+			</PortfolioBoxes>
+			<PortfolioBoxes className="links" id="frontend">
+				<PortfolioBoxTitle>
+					Sites that utilize only a front end
+				</PortfolioBoxTitle>
+			</PortfolioBoxes>
+			<PortfolioBoxes className="links" id="backend">
+				<PortfolioBoxTitle>
+					Sites that utilize only a back end
+					<p>(links go to video demonstrations)</p>
+				</PortfolioBoxTitle>
+			</PortfolioBoxes>
 		</Container>
 	)
 }
